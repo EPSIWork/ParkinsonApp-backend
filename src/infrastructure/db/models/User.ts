a0a2537a6ecaa070567673513f533fa9771c11c6
Elauriche Nickson <52778200+3l4un1ck@users.noreply.github.com>
2024-10-11 11:58:58 +0200

    build all features

--- src/infrastructure/db/models/User.ts
+++ src/infrastructure/db/models/User.ts
@@ -15,6 +15,10 @@ const User = sequelize.define('User', {
         type: DataTypes.STRING,
         allowNull: true,
     },
+    address:{
+        type: DataTypes.STRING,
+        allowNull:true,
+    },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
